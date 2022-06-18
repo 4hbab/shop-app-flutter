6bdf6b9814c1abb8873c6e2b7b27a6759719c5ed
ahbab <sakifahbab2@iut-dhaka.edu>
2022-06-18 13:06:28 +0600

    added database to the orders part

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -8,6 +11,11 @@ class Product with ChangeNotifier {
   final String imageUrl;
   bool isFavorite;
 
+  void _setFavValue(bool newVal) {
+    isFavorite = newVal;
+    notifyListeners();
+  }
+
   Product(
       {required this.id,
       required this.title,
@@ -16,8 +24,24 @@ class Product with ChangeNotifier {
       required this.imageUrl,
       this.isFavorite = false});
 
-  void toggleFavoriteStatus() {
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url = Uri.parse(
+        'https://flutter-shop-app-4fa31-default-rtdb.firebaseio.com//products/$id.json');
+    try {
+      final response = await http.patch(url,
+          body: json.encode(
+            {
+              'isFavorite': isFavorite,
+            },
+          ));
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (err) {
+      _setFavValue(oldStatus);
+    }
   }
 }
